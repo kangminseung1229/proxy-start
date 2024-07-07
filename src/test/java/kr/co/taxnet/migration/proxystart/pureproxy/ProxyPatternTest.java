b112a1b78849d3a65956a883155e693e0f8ecd4a
kangminseung <msk109@inaus.co.kr>
2024-07-07 16:14:50 +0900

    프록시 예제 2

--- src/test/java/kr/co/taxnet/migration/proxystart/pureproxy/ProxyPatternTest.java
+++ src/test/java/kr/co/taxnet/migration/proxystart/pureproxy/ProxyPatternTest.java
@@ -12,4 +12,15 @@ public class ProxyPatternTest {
         client.execute();
         client.execute();
     }
+
+
+    @Test
+    void cacheProxyTest() {
+        Subject realSubject = new RealSubject();
+        Subject cacheProxy = new CacheProxy(realSubject);
+        ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
+        client.execute();
+        client.execute();
+        client.execute();
+    }
 }
