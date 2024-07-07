299c45e0f46a6e2f9713760bb0bee9b5bfbdf2a7
kangminseung <msk109@inaus.co.kr>
2024-07-07 15:38:53 +0900

    프록시 패턴 v2

--- src/main/java/kr/co/taxnet/migration/proxystart/ProxyStartApplication.java
+++ src/main/java/kr/co/taxnet/migration/proxystart/ProxyStartApplication.java
@@ -1,13 +1,14 @@
 package kr.co.taxnet.migration.proxystart;
 
 import kr.co.taxnet.migration.proxystart.hello.proxy.config.AppV1Config;
+import kr.co.taxnet.migration.proxystart.hello.proxy.config.AppV2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 
-@Import(AppV1Config.class)
-@SpringBootApplication(scanBasePackages = "hello.proxy.app.v3")
+@Import({AppV1Config.class, AppV2Config.class})
+@SpringBootApplication(scanBasePackages = "hello.proxy.app")
 public class ProxyStartApplication {
 
     public static void main(String[] args) {
