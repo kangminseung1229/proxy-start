6f2d8a1bd5e95148ce3dee52b3d46c13b3b076e1
kangminseung <msk109@inaus.co.kr>
2024-07-26 16:47:02 +0900

    구체클래스 기반 프록시 1

---
+++ src/test/java/proxy/start/concreteproxy/code/ConcreteClient.java
@@ -0,0 +1,14 @@
+package proxy.start.concreteproxy.code;
+
+public class ConcreteClient {
+
+    private ConcreteLogic concreteLogic;
+
+    public ConcreteClient(ConcreteLogic concreteLogic){
+        this.concreteLogic = concreteLogic;
+    }
+
+    public void excute(){
+        concreteLogic.operation();
+    }
+}
---
+++ src/test/java/proxy/start/concreteproxy/code/ConcreteProxyTest.java
@@ -0,0 +1,13 @@
+package proxy.start.concreteproxy.code;
+
+import org.junit.jupiter.api.Test;
+
+public class ConcreteProxyTest {
+
+    @Test
+    void noProxy(){
+        ConcreteLogic concreteLogic = new ConcreteLogic();
+        ConcreteClient concreteClient = new ConcreteClient(concreteLogic);
+        concreteClient.excute();
+    }
+}
