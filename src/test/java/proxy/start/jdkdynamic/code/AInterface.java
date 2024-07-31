63850c49e8eae12cc8b2ab7e3f9e2c71f94d55e3
kangminseung <msk109@inaus.co.kr>
2024-07-31 09:38:29 +0900

    jdkdynamicproxy 예제

---
+++ src/test/java/proxy/start/jdkdynamic/code/AInterface.java
@@ -0,0 +1,5 @@
+package proxy.start.jdkdynamic.code;
+
+public interface AInterface {
+    String call();
+}
---
+++ src/test/java/proxy/start/jdkdynamic/code/BInterface.java
@@ -0,0 +1,5 @@
+package proxy.start.jdkdynamic.code;
+
+public interface BInterface {
+    String call();
+}
