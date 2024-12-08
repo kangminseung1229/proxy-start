f3291299c7b36f8b9e4cce38609e3ab3ffcad1da
kangminseung <msk109@inaus.co.kr>
2024-12-08 17:39:41 +0900

    멀티프록시 정리

--- src/test/java/proxy/start/advisor/MultiAdvisorTest.java
+++ src/test/java/proxy/start/advisor/MultiAdvisorTest.java
@@ -18,19 +18,26 @@ public class MultiAdvisorTest {
     @DisplayName("여러 프록시")
     void multiAdvisorTest1() {
         //client -> proxy2(advisor2) -> proxy1(advisor1) -> target
-//프록시1 생성
+
+
+        //프록시1 생성
         ServiceInterface target = new ServiceImpl();
         ProxyFactory proxyFactory1 = new ProxyFactory(target);
+
         DefaultPointcutAdvisor advisor1 = new
                 DefaultPointcutAdvisor(Pointcut.TRUE, new Advice1());
+
         proxyFactory1.addAdvisor(advisor1);
         ServiceInterface proxy1 = (ServiceInterface) proxyFactory1.getProxy();
-//프록시2 생성, target -> proxy1 입력
+
+        //프록시2 생성, target -> proxy1 입력
         ProxyFactory proxyFactory2 = new ProxyFactory(proxy1);
         DefaultPointcutAdvisor advisor2 = new
                 DefaultPointcutAdvisor(Pointcut.TRUE, new Advice2());
+
         proxyFactory2.addAdvisor(advisor2);
         ServiceInterface proxy2 = (ServiceInterface) proxyFactory2.getProxy(); //실행
+
         proxy2.save();
     }
 
